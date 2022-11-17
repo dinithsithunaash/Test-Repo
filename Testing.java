4606bfb9e8fdcf084aae7a63f56bda325c53dea2
dinithsithunaash <45732100+dinithsithunaash@users.noreply.github.com>
2022-11-17 12:13:10 +0530

    Modified with s.out

--- Testing.java
+++ Testing.java
@@ -1,5 +1,6 @@
 public class Testing{
 	public static void main(String args[]){
-		System.out.println("Hello World Test");
+		System.out.println("Hello World Test\n");
+		System.out.println("Running after the changes");
 	}
 }
